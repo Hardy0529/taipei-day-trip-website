b8d619f542ce9ef62b75de16d357eb767fe197a9
李杰儒 Jeff <ijieru41@gmail.com>
2022-03-29 18:41:58 +0800

    更新資料

--- static/js/bookingCheckBtn.js
+++ static/js/bookingCheckBtn.js
@@ -1,105 +1,58 @@
-function init() {
-  let booking_Check_btn = document.querySelector("#booking_Check_btn");
-  booking_Check_btn.addEventListener("click", function () {
-    let salepage__form_date = document.querySelector(
-      "#salepage__form-date"
-    ).value;
-    let attractionId = location.pathname.replace("/attraction/", "");
-    let selected = document.querySelector("input[type='radio']:checked").value;
+let booking_Check_btn = document.querySelector("#booking_Check_btn");
 
-    let priceData = "";
-    if (selected == "afternoon") {
-      priceData = 2000;
-    } else if (selected == "evening") {
-      priceData = 2500;
-    }
-
-    console.log(salepage__form_date);
-    console.log(attractionId);
-    console.log(selected);
-    console.log(priceData);
-
-    const bookingData = {
-      attractionId: attractionId,
-      date: salepage__form_date,
-      time: selected,
-      price: priceData,
-    };
-
-    fetch("/api/booking", {
-      method: "POST", // or 'PUT'
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(bookingData),
+// 按鈕 開始預定行程
+let popupBg = document.querySelector(".popupBg");
+booking_Check_btn.addEventListener("click", function () {
+  fetch("/api/user")
+    .then(function (response) {
+      return response.json();
     })
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (result) {
-        console.log(result);
-      });
+    .then(function (result) {
+      if (result.data == null) {
+        popupBg.setAttribute("class", "popupBg popupBg__SHOW");
+      } else if (result.data != null) {
+        let salepage__form_date = document.querySelector(
+          "#salepage__form-date"
+        ).value;
+        let attractionId = location.pathname.replace("/attraction/", "");
+        let selected = document.querySelector(
+          "input[type='radio']:checked"
+        ).value;
 
-    document.location = "/booking";
-  });
-  // --------------------------------------
-  // let booking_Check_btn = document.querySelector("#booking_Check_btn");
+        let priceData = "";
+        if (selected == "afternoon") {
+          priceData = 2000;
+        } else if (selected == "evening") {
+          priceData = 2500;
+        }
 
-  // // 按鈕 開始預定行程
-  // let popupBg = document.querySelector(".popupBg");
-  // booking_Check_btn.addEventListener("click", function () {
-  //   fetch("/api/user")
-  //     .then(function (response) {
-  //       return response.json();
-  //     })
-  //     .then(function (result) {
-  //       if (result.data == null) {
-  //         popupBg.setAttribute("class", "popupBg popupBg__SHOW");
-  //       } else if (result.data != null) {
-  //         let salepage__form_date = document.querySelector(
-  //           "#salepage__form-date"
-  //         ).value;
-  //         let attractionId = location.pathname.replace("/attraction/", "");
-  //         let selected = document.querySelector(
-  //           "input[type='radio']:checked"
-  //         ).value;
+        console.log(salepage__form_date);
+        console.log(attractionId);
+        console.log(selected);
+        console.log(priceData);
 
-  //         let priceData = "";
-  //         if (selected == "afternoon") {
-  //           priceData = 2000;
-  //         } else if (selected == "evening") {
-  //           priceData = 2500;
-  //         }
+        const bookingData = {
+          attractionId: attractionId,
+          date: salepage__form_date,
+          time: selected,
+          price: priceData,
+        };
 
-  //         console.log(salepage__form_date);
-  //         console.log(attractionId);
-  //         console.log(selected);
-  //         console.log(priceData);
+        fetch("/api/booking", {
+          method: "POST", // or 'PUT'
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(bookingData),
+        })
+          .then(function (response) {
+            return response.json();
+          })
+          .then(function (result) {
+            console.log(result);
+          });
 
-  //         const bookingData = {
-  //           attractionId: attractionId,
-  //           date: salepage__form_date,
-  //           time: selected,
-  //           price: priceData,
-  //         };
-
-  //         fetch("/api/booking", {
-  //           method: "POST", // or 'PUT'
-  //           headers: {
-  //             "Content-Type": "application/json",
-  //           },
-  //           body: JSON.stringify(bookingData),
-  //         })
-  //           .then(function (response) {
-  //             return response.json();
-  //           })
-  //           .then(function (result) {
-  //             console.log(result);
-  //           });
-
-  //         document.location = "/booking";
-  //       }
-  //     });
-  // });
-}
-init();
+        document.location = "/booking";
+      }
+    });
+});
