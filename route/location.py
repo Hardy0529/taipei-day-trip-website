c111ea62724c2780d7baaa6890bb545fcadd3b90
李杰儒 Jeff <ijieru41@gmail.com>
2022-04-21 10:56:52 +0800

    updata

--- route/location.py
+++ route/location.py
@@ -21,7 +21,7 @@ def index():
     my_lon = location[1]
     min_result = 999
     result_name = "台北"
-
+    print(my_lat, my_lon)
     url = "https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-D0047-091?Authorization=CWB-EF46CCF9-7FB5-4120-AA40-CFDDA8BC249C"
     response = requests.get(url)
     data = json.loads(response.text)
@@ -29,10 +29,10 @@ def index():
     for city in data["records"]["locations"][0]["location"]:
         lat = float(city["lat"])
         lon = float(city["lon"])
-        print(lat, lon)
+        # print(lat, lon)
         result = math.sqrt(math.pow((my_lat - lat), 2) +
                            math.pow((my_lon - lon), 2))
-        print(result)
+        # print(result)
 
         if min_result > result:
             min_result = result
